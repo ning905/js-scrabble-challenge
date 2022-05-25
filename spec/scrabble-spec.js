2247229722dcd613ea932db1d3dcd2a4aced9d1b
Ning Ma <ningma905@gmail.com>
2022-05-25 15:59:13 +0100

    add test and a second solution

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -3,52 +3,72 @@
 Scrabble = require('../src/scrabble')
 let scrabble;
 
-describe("Scrabble", function() {
-  it('returns 0 for empty words', function() {
+describe("Scrabble", function () {
+  it('returns 0 for empty words', function () {
     scrabble = new Scrabble('')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for whitespace', function() {
+  it('returns 0 for whitespace', function () {
     scrabble = new Scrabble(" \t\n")
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for null', function() {
+  it('returns 0 for null', function () {
     scrabble = new Scrabble(null)
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('a')
 
     expect(scrabble.score()).toEqual(1)
   })
 
-  it('scores short word', function() {
+  it('scores short word', function () {
     scrabble = new Scrabble('f')
 
     expect(scrabble.score()).toEqual(4)
   })
 
-  it('scores a simple word', function() {
+  it('scores a simple word', function () {
     scrabble = new Scrabble('street')
 
     expect(scrabble.score()).toEqual(6)
   })
 
-  it('scores a more complicated word', function() {
+  it('scores a more complicated word', function () {
     scrabble = new Scrabble('quirky')
 
     expect(scrabble.score()).toEqual(22)
   })
 
-  it('scores a case-insensitive word', function() {
+  it('scores a case-insensitive word', function () {
     scrabble = new Scrabble('OXYPHENBUTAZONE')
 
     expect(scrabble.score()).toEqual(41)
   })
+
+  it('scores a word with doubled letters', function () {
+    scrabble = new Scrabble('str{ee}t')
+
+    expect(scrabble.score()).toEqual(8)
+  })
+
+  it('scores a word with tripled letters', function () {
+    scrabble = new Scrabble('str[ee]t')
+
+    expect(scrabble.score()).toEqual(10)
+  })
+
+  it('scores a word with doubled and tripled letters', function () {
+    scrabble = new Scrabble('st{r}[ee]t')
+
+    expect(scrabble.score()).toEqual(11)
+  })
+
+
 })
